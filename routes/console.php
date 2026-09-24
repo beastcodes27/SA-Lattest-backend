438fc47ea4ed4165933697d472346b3fbdc336e5
imani <beastcodes27@gmail.com>
2026-09-24 23:34:50 +0300

    chore(schedule): register leave:expire-pending to run hourly via Laravel scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/*
+|--------------------------------------------------------------------------
+| Scheduled Commands
+|--------------------------------------------------------------------------
+| Run every hour to auto-expire unanswered leave requests and send
+| 24-hour pre-expiry reminders to org admins.
+*/
+Schedule::command('leave:expire-pending')->hourly()->withoutOverlapping();
